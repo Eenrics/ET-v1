f1252abed66c3a800c5274204fdbb7cac0519343
bilalabdelkadir <bilalbinabdelkadir>
2023-02-27 16:42:07 +0300

    i created register controller to handle signup

---
+++ server/controllers/registerController.js
@@ -0,0 +1,51 @@
+import User from "../models/User.js";
+import createToken from "../utils/CreateToken.js";
+import nodemailer from "nodemailer";
+
+const registerUser = async (req, res) => {
+  const { email, password, phoneNumber } = req.body;
+
+  try {
+    const user = await User.register(email, password, phoneNumber);
+    const token = createToken(user._id);
+
+    // send email verification
+    const verificationToken = jwt.sign(
+      { email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    user.verificationToken = verificationToken;
+    user.verificationTokenExpiration = Date.now() + 60 * 60 * 1000; // 1 hour
+
+    // send verification email
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: user.email,
+      subject: "Email verification for your account",
+      html: `Please click this link to verify your email address: <a href="${process.env.BASE_URL}/api/v1/verify-email?token=${verificationToken}">Verify Email</a>`,
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    });
+    await user.save();
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default registerUser;
